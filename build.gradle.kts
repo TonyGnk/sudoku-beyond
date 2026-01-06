1f044d60ae6cb58083d2d3e038602c1a1ddd8723
TonyGnk <iis22067@uom.edu.gr>
2026-01-06 14:18:45 +0200

    [P3-Cleanup-Hilt-And-Accompanist-Remove] Remove unnecessary suppressions from build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,8 +16,6 @@
  * GNU General Public License for more details.
  */
 
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
@@ -26,4 +24,3 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.jetbrains.kotlin.compose) apply false
 }
-true // Needed to make the Suppress annotation work for the plugins block
